98a9d681065684b2fe32b4600a9d4c2bf0310b66
danimpulse500 <danimpulse500@gmail.com>
2026-01-06 12:58:17 +0100

    fixed static url

--- config/settings.py
+++ config/settings.py
@@ -224,11 +224,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # Optional: if you have additional static directories
-]
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATICFILES_DIRS = []
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
